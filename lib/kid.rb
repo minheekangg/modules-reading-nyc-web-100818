2a5b874371e715ab07df18a62d8442ee13ebd36f
Min Hee  Park <minheekangg@gmail.com>
2018-10-16 21:28:46 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,7 @@ require_relative './class_methods_module.rb'
 
 class Kid
   extend FancyDance::ClassMethods
- include FancyDance::InstanceMethods
+  include FancyDance::InstanceMethods
 
 attr_accessor :name
 
